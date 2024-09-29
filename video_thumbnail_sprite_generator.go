5f3ed6b8142349284fc3e92c23da96985db74869
karoly.bodei <karoly.bodei@trendency.hu>
2024-09-29 14:06:19 +0200

    feat: Add storyboard calculation and introduce consistent naming strategy

--- video_thumbnail_sprite_generator.go
+++ video_thumbnail_sprite_generator.go
@@ -6,33 +6,48 @@ import (
 	"errors"
 	"fmt"
 	ffmpeg "github.com/u2takey/ffmpeg-go"
+	"math"
 	"strconv"
 )
 
-type Metadata struct {
+type VideoMetadata struct {
 	Width    int
 	Height   int
-	Duration int
+	Duration float64
 }
 
-func GetMetadata(fileName string) (Metadata, error) {
+type StoryboardMetadata struct {
+	Url        string                   `json:"url"`
+	TileWidth  int                      `json:"tile_width"`
+	TileHeight int                      `json:"tile_height"`
+	Duration   float64                  `json:"duration"`
+	Tiles      []StoryboardMetadataTile `json:"tiles"`
+}
+
+type StoryboardMetadataTile struct {
+	Start float64 `json:"start"`
+	X     int     `json:"x"`
+	Y     int     `json:"y"`
+}
+
+func GetMetadata(fileName string) (VideoMetadata, error) {
 	probe, err := ffmpeg.Probe(fileName)
 
 	if err != nil {
-		return Metadata{}, fmt.Errorf("error fetching metadata: %s", err.Error())
+		return VideoMetadata{}, fmt.Errorf("error fetching metadata: %s", err.Error())
 	}
 
 	var metadata map[string]interface{}
 	err = json.Unmarshal([]byte(probe), &metadata)
 
 	if err != nil {
-		return Metadata{}, fmt.Errorf("error parsing metadata: %s", err.Error())
+		return VideoMetadata{}, fmt.Errorf("error parsing metadata: %s", err.Error())
 	}
 
 	duration, err := strconv.ParseFloat(metadata["format"].(map[string]interface{})["duration"].(string), 64)
 
 	if err != nil {
-		return Metadata{}, fmt.Errorf("error parsing duration: %s", err.Error())
+		return VideoMetadata{}, fmt.Errorf("error parsing duration: %s", err.Error())
 	}
 
 	streams := metadata["streams"].([]interface{})
@@ -41,18 +56,18 @@ func GetMetadata(fileName string) (Metadata, error) {
 		streamInfo := stream.(map[string]interface{})
 
 		if streamInfo["codec_type"] == "video" {
-			return Metadata{
+			return VideoMetadata{
 				Width:    int(streamInfo["width"].(float64)),
 				Height:   int(streamInfo["height"].(float64)),
-				Duration: int(duration),
+				Duration: duration,
 			}, nil
 		}
 	}
 
-	return Metadata{}, errors.New("could not find video stream")
+	return VideoMetadata{}, errors.New("could not find video stream")
 }
 
-func CalculateFrameDimensions(width int, height int, maxWidth int, maxHeight int) (int, int, error) {
+func CalculateTileDimensions(width int, height int, maxWidth int, maxHeight int) (int, int, error) {
 	highestDivisor := min(width, height) / 2
 
 	for i := 2; i < highestDivisor; i++ {
@@ -78,7 +93,7 @@ func CalculateFrameDimensions(width int, height int, maxWidth int, maxHeight int
 	return 0, 0, errors.New("could not find divisor within maxWidth and maxHeight")
 }
 
-func GenerateSprite(fileName string, interval int, columns int, rows int, frameWidth int, frameHeight int) (*bytes.Buffer, error) {
+func GenerateStoryboardImage(fileName string, interval int, columns int, rows int, tileWidth int, tileHeight int) (*bytes.Buffer, error) {
 	buffer := bytes.NewBuffer(nil)
 
 	err := ffmpeg.
@@ -86,7 +101,7 @@ func GenerateSprite(fileName string, interval int, columns int, rows int, frameW
 		Filter("fps", ffmpeg.Args{
 			fmt.Sprintf("1/%d", interval),
 		}).
-		Filter("scale", ffmpeg.Args{strconv.Itoa(frameWidth), strconv.Itoa(frameHeight)}).
+		Filter("scale", ffmpeg.Args{strconv.Itoa(tileWidth), strconv.Itoa(tileHeight)}).
 		Filter("tile", ffmpeg.Args{}, ffmpeg.KwArgs{
 			"layout": fmt.Sprintf("%dx%d", columns, rows),
 		}).
@@ -100,4 +115,34 @@ func GenerateSprite(fileName string, interval int, columns int, rows int, frameW
 		Run()
 
 	return buffer, err
+}
+
+func GenerateStoryboardMetadata(url string, interval int, columns int, tileWidth int, tileHeight int, duration float64) StoryboardMetadata {
+	tiles := make([]StoryboardMetadataTile, 0)
+
+	tileCount := int(math.Ceil(duration / float64(interval)))
+
+	for i := 0; i < tileCount; i++ {
+		start := interval * i
+
+		column := i % columns
+		row := i / columns
+
+		x := column * tileWidth
+		y := row * tileHeight
+
+		tiles = append(tiles, StoryboardMetadataTile{
+			Start: float64(start),
+			X:     x,
+			Y:     y,
+		})
+	}
+
+	return StoryboardMetadata{
+		Url:        url,
+		TileWidth:  tileWidth,
+		TileHeight: tileHeight,
+		Duration:   duration,
+		Tiles:      tiles,
+	}
 }
